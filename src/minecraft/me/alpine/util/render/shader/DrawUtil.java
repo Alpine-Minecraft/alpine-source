35e3a5a307fdd095054214d3b7448c4bbf300258
iSTeeWx <vdsti2006@gmail.com>
2022-08-06 00:01:16 +0200

    Added blur util (from ketamin lmao)

---
+++ src/minecraft/me/alpine/util/render/shader/DrawUtil.java
@@ -0,0 +1,67 @@
+package me.alpine.util.render.shader;
+
+import lombok.experimental.UtilityClass;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL14.glBlendFuncSeparate;
+
+/**
+ * Taken from ketamine
+ */
+@UtilityClass
+public final class DrawUtil {
+
+    public static void glColour(final int color) {
+        glColor4ub((byte) (color >> 16 & 0xFF),
+                (byte) (color >> 8 & 0xFF),
+                (byte) (color & 0xFF),
+                (byte) (color >> 24 & 0xFF));
+    }
+
+    public static void glDrawFilledQuad(final double x,
+                                        final double y,
+                                        final double width,
+                                        final double height,
+                                        final int colour) {
+        // Enable blending
+        final boolean restore = glEnableBlend();
+        // Disable texture drawing
+        glDisable(GL_TEXTURE_2D);
+        // Set color
+        glColour(colour);
+
+        // Begin rect
+        glBegin(GL_QUADS);
+        {
+            glVertex2d(x, y);
+            glVertex2d(x, y + height);
+            glVertex2d(x + width, y + height);
+            glVertex2d(x + width, y);
+        }
+        // Draw the rect
+        glEnd();
+
+        // Disable blending
+        glRestoreBlend(restore);
+        // Re-enable texture drawing
+        glEnable(GL_TEXTURE_2D);
+    }
+
+
+    public static boolean glEnableBlend() {
+        final boolean wasEnabled = glIsEnabled(GL_BLEND);
+
+        if (!wasEnabled) {
+            glEnable(GL_BLEND);
+            glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, 1, 0);
+        }
+
+        return wasEnabled;
+    }
+
+    public static void glRestoreBlend(final boolean wasEnabled) {
+        if (!wasEnabled) {
+            glDisable(GL_BLEND);
+        }
+    }
+}
