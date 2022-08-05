279b5910ec6243ef84b86a7c4217157e367e5894
iSTeeWx <vdsti2006@gmail.com>
2022-08-05 18:07:54 +0200

    Organised a bit Alpine.java

--- src/minecraft/me/alpine/Alpine.java
+++ src/minecraft/me/alpine/Alpine.java
@@ -1,39 +1,54 @@
 package me.alpine;
 
 import lombok.Getter;
+import lombok.Setter;
 import me.alpine.event.EventManager;
 import me.alpine.mod.ModsManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+@Getter @Setter
 public class Alpine {
     /** The instance of the client */
     @Getter private static final Alpine instance = new Alpine();
 
     /** The client name */
-    @Getter private final String name = "Alpine";
+    private final String name;
     /** The client version */
-    @Getter private final String version = "0.1";
+    private final String version;
+    /** The client owners */
+    private final String[] owners;
     /** The Logger implementation */
-    @Getter private final Logger logger = LogManager.getLogger("Alpine");
+    private final Logger logger;
 
-    @Getter private final ModsManager modsManager = new ModsManager();
+    /** The mods manager instance */
+    private final ModsManager modsManager;
+
+    private Alpine() {
+        this.name = "Alpine";
+        this.version = "0.1";
+        this.owners = new String[]{"iSTeeWx_", "Audi"};
+
+        this.logger = LogManager.getLogger("Alpine");
+
+        this.modsManager = new ModsManager();
+    }
 
     /**
      * This is the method that will be called when Minecraft starts.
      */
     public void onStart() {
+        logger.info("[Alpine] Starting Alpine on version " + version);
+
         /* This adds a shutdown hook to the JVM that will call this.onStop. */
         Runtime.getRuntime().addShutdownHook(new Thread(this::onStop));
-
-        logger.info("[Alpine] Starting Alpine on version " + version);
     }
 
     /**
      * This is the method that will be called when Minecraft stops.
      */
     public void onStop() {
-        EventManager.clean();
         logger.info("[Alpine] Stopping!");
+        EventManager.clean();
     }
 }
