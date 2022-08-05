df52bee24cfa5a1741454da7d0129ca6d388ab61
FrenchToyota <glasssquaredlol@gmail.com>
2022-08-05 23:16:50 +0100

    Blur and Watermark

---
+++ src/minecraft/me/alpine/mod/impl/ModWatermark.java
@@ -0,0 +1,26 @@
+package me.alpine.mod.impl;
+
+import me.alpine.Alpine;
+import me.alpine.event.EventTarget;
+import me.alpine.event.impl.EventRender2D;
+import me.alpine.mod.EnumModCategory;
+import me.alpine.mod.Mod;
+import me.alpine.util.render.shader.BlurUtil;
+import net.minecraft.client.gui.ScaledResolution;
+
+public class ModWatermark extends Mod {
+    public ModWatermark() {
+        super("Watermark", "A descriptive description", EnumModCategory.HUD);
+        this.setEnabled(true);
+    }
+
+    @EventTarget
+    public void onRender2D(EventRender2D e) {
+        ScaledResolution scaledResolution = new ScaledResolution(mc);
+        double y = scaledResolution.getScaledHeight() - 10;
+        BlurUtil.blurArea(0, 20, mc.fontRendererObj.getStringWidth("  Alpine " + Alpine.getInstance().getVersion()), 20);
+        mc.fontRendererObj.drawStringWithShadow("  ALPINE " + Alpine.getInstance().getVersion(), (float) 0, (float) y, 0x00fff7);
+
+
+    }
+}
