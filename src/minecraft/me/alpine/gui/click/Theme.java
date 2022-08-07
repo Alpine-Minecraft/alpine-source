e3a0fada8385ff51e42b9049ae029fa2f5b47ca7
iSTeeWx <vdsti2006@gmail.com>
2022-08-08 01:15:59 +0200

    Added very basic theme stuff

--- src/minecraft/me/alpine/gui/click/Theme.java
+++ src/minecraft/me/alpine/gui/click/Theme.java
@@ -4,11 +4,11 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Theme {
-    static int getBackgroundColor() {
+    public static int getBackgroundColor() {
         return 0xFF202030;
     }
 
-    static int getTrimColor() {
+    public static int getTrimColor() {
         return 0xFF000000;
     }
 }
