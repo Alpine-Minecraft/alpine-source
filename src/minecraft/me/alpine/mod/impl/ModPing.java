a02d502f734f6c1c3b88b62fbb78f38bd44d60c5
iSTeeWx <vdsti2006@gmail.com>
2022-08-07 20:25:33 +0200

    Removed big ugly one-liner

--- src/minecraft/me/alpine/mod/impl/ModPing.java
+++ src/minecraft/me/alpine/mod/impl/ModPing.java
@@ -4,22 +4,23 @@ import me.alpine.event.EventTarget;
 import me.alpine.event.impl.EventRender2D;
 import me.alpine.mod.EnumModCategory;
 import me.alpine.mod.Mod;
-import net.minecraft.client.Minecraft;
 
 public class ModPing extends Mod {
     public ModPing() {
-        super("Ping", "Pings", EnumModCategory.PLAYER);
+        super("Ping", "Renders the server ping", EnumModCategory.PLAYER);
     }
 
-    public String getPlayerPing() {
-        return Minecraft.getMinecraft().isSingleplayer() ? "-1" : Minecraft.getMinecraft().getNetHandler().getPlayerInfo(Minecraft.getMinecraft().thePlayer.getUniqueID()) == null ? "0000" : String.valueOf(Minecraft.getMinecraft().getNetHandler().getPlayerInfo(Minecraft.getMinecraft().thePlayer.getUniqueID()).getResponseTime());
+    private String getPing() {
+        if (mc.isSingleplayer()) return "-1";
+        if (mc.getNetHandler().getPlayerInfo(mc.thePlayer.getUniqueID()) == null) return "0000";
+        return mc.getNetHandler().getPlayerInfo(mc.thePlayer.getUniqueID()).getResponseTime() + "";
     }
 
     @EventTarget
     public void onRender2D(EventRender2D e) {
         int x = e.getSr().getScaledWidth() - 100;
         int y = 2;
-        String ping = String.format("Ping: %s", getPlayerPing());
+        String ping = String.format("Ping: %s", getPing());
         mc.fontRendererObj.drawStringWithShadow(ping, x, y, 0x00d9ff);
     }
 
