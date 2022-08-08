bd0f5d3cf51c55ad518e745060e0d262d1bb77ac
iSTeeWx <vdsti2006@gmail.com>
2022-08-08 14:13:02 +0200

    Added color setting to mod test

--- src/minecraft/me/alpine/mod/impl/ModTest.java
+++ src/minecraft/me/alpine/mod/impl/ModTest.java
@@ -7,14 +7,17 @@ import me.alpine.mod.property.impl.ComboProperty;
 import me.alpine.mod.property.impl.EnumProperty;
 import me.alpine.mod.property.impl.NumberProperty;
 
+import java.awt.*;
+
 public class ModTest extends Mod {
     public ModTest() {
         super("TEST MOD", "This is a test mod", EnumModCategory.MISC);
 
         addProperty("Boolean Property", true);
         addProperty("Number Property", 0.0, 0.0, 1.0, 0.1);
-        addProperty("Combo Property", "Value 1", "Value 1", "Value 2", "Value 3");
-        addProperty("Enum Property", new String[]{"Value 1", "Value 3"}, new String[]{"Value 1", "Value 2", "Value 3"});
+        addProperty("Color Property", Color.WHITE);
+        addProperty("Enum Property", "Value 1", "Value 1", "Value 2", "Value 3");
+        addProperty("Combo Property", new String[]{"Value 1", "Value 3"}, new String[]{"Value 1", "Value 2", "Value 3"});
         addProperty("Folder Property",
                 new BooleanProperty("Boolean Property", true),
                 new NumberProperty("Number Property", 0.0, 0.0, 1.0, 0.1),
