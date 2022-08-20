5d88d40971c56767ddc70c470a1cfc9dfc64a3f4
FrenchToyota <glasssquaredlol@gmail.com>
2022-08-20 16:34:41 +0100

    Added Auto GG Mod

--- src/minecraft/me/alpine/mod/impl/ModAutoGG.java
+++ src/minecraft/me/alpine/mod/impl/ModAutoGG.java
@@ -1,7 +1,11 @@
 package me.alpine.mod.impl;
 
+import me.alpine.event.EventTarget;
+import me.alpine.event.impl.EventPacket;
 import me.alpine.mod.EnumModCategory;
 import me.alpine.mod.Mod;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S02PacketChat;
 
 public class ModAutoGG extends Mod {
     public ModAutoGG() {
@@ -11,6 +15,17 @@ public class ModAutoGG extends Mod {
     @Override
     public void onEnable() {
         super.onEnable();
+    }
+
+    @EventTarget
+    public void onPacket(EventPacket e){
+        Packet packet = e.getPacket();
+        if(packet instanceof S02PacketChat){
+            S02PacketChat chat = (S02PacketChat) packet;
+            if(chat.getChatComponent().getUnformattedText().equals("Want to play again?")){
+                mc.thePlayer.sendChatMessage("gg");
+            }
+        }
     }
 }
 
